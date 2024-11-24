83d651dfe9681e30263da5472ccafd10ebf85a4b
jycouet <jycouet@gmail.com>
2024-11-24 13:26:05 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { PUBLIC_ENV1 } from "$env/static/public";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+{PUBLIC_ENV1}
