88020f4eea19d1f3b80fc17dddc56212749ee158
jycouet <jycouet@gmail.com>
2024-12-02 14:34:50 +0100

    new tests

--- src/routes/envs/+page.server.ts
+++ src/routes/envs/+page.server.ts
@@ -1,10 +1,13 @@
 import { env } from "$env/dynamic/private";
-import { HELLO } from "$env/static/private";
+import { HELLO, NEW_ENV_IN_UI } from "$env/static/private";
+import { PUBLIC_ENV1 } from "$env/static/public";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async () => {
   console.log("Envs?", env);
   return {
     HELLO,
+    PUBLIC_ENV1,
+    NEW_ENV_IN_UI,
   };
 }) satisfies PageServerLoad;
